0ca6a3903baa965be5c3629193f610179c16db35
arnedb <arne.debrabandere@student.kuleuven.be>
2015-02-21 20:58:38 +0100

    ExpFeatureSet: update voor cross-validatie

---
+++ ActivityRecognition/src/CrossValidation.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+import weka.core.Instances;
+
+public class CrossValidation {
+	
+	public static void crossValidation(Method method, DataSet data, int folds, int seed) {
+		
+		Random rand = new Random(seed);
+		Instances randData = new Instances(data.instances);
+		randData.randomize(rand);
+		
+		//randData.stratify(folds);
+		
+		for (int n = 0; n < folds; n++) {
+			  Instances train = randData.trainCV(folds, n);
+			  Instances test = randData.testCV(folds, n);
+			 
+			  // further processing, classification, etc.
+		}
+		
+	}
+
+}
