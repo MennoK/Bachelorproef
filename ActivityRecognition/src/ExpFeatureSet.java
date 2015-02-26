32bb8013fcd0b52e89a0c594e8bb5011ce094279
arnedb <arne.debrabandere@student.kuleuven.be>
2015-02-26 19:12:27 +0100

    Experimenten feature set

--- ActivityRecognition/src/ExpFeatureSet.java
+++ ActivityRecognition/src/ExpFeatureSet.java
@@ -19,22 +19,45 @@ import weka.core.Instances;
  */
 public class ExpFeatureSet {
 	
-	public static String exp = "./Experimenten/FeatureSet/InfoGain/RandomForest"; // map waaronder alle resultaten bewaard worden
-	public static String data = "./Experimenten/FeatureSet/data-allfeatures.csv"; // pad naar dataset (.csv)
+	/**
+	 * Map waaronder alle resultaten bewaard worden.
+	 */
+	public static String exp = "./Experimenten/FeatureSet/FFT/InfoGain/RandomForest/";
+	
+	/**
+	 * Pad naar dataset (.csv)
+	 */
+	public static String data = "./Experimenten/FeatureSet/data-fft.csv";
+	
+	/**
+	 * Totaal aantal features in de dataset.
+	 */
+	// public static int TOTAL_NUM_FEATURES = 134; // data-allfeatures.csv
+	// public static int TOTAL_NUM_FEATURES = 10; // data-hmm.csv
+	// public static int TOTAL_NUM_FEATURES = 20; // data-statistic.csv
+	// public static int TOTAL_NUM_FEATURES = 30; // data-dwt.csv
+	public static int TOTAL_NUM_FEATURES = 84; // data-fft.csv
+	
+	/**
+	 * Gebruikte methode (met parameters)
+	 */
+	// public static String method = "weka.classifiers.trees.J48 -- -C 0.25 -M 2";
+	public static String method = "weka.classifiers.trees.RandomForest -- -I 10 -K 0 -S 1";
+	
+	/**
+	 * Opties voor AttributeSelectedClassifier (zonder de parameters -N en -W)
+	 */
+	public static String options = "-x 2 -E \"weka.attributeSelection.InfoGainAttributeEval\" -S \"weka.attributeSelection.Ranker";
+	// public static String options = "-x 2 -E \"weka.attributeSelection.ChiSquaredAttributeEval\" -S \"weka.attributeSelection.Ranker";
 
 	public static void main(String[] args) throws Exception {
 		
 		// importeer data
 		DataSet dataset = new DataSet(data);
 		
-		// kies een methode
-		// String method = "weka.classifiers.trees.J48 -- -C 0.25 -M 2";
-		// String method = "weka.classifiers.trees.RandomForest -- -I 10 -K 0 -S 1";
-		String method = "";
-		
 		// evalueer methode voor verschillende aantallen features met 10-fold cross-validatie
 		String output = "";
-		for (int numFeatures = 1; numFeatures <= 134; numFeatures += 1) {
+		for (int numFeatures = 1; numFeatures <= TOTAL_NUM_FEATURES; numFeatures += 1) {
 			
 		    double accuracy = 0.0;
 		    
@@ -52,9 +75,8 @@ public class ExpFeatureSet {
 				 
 				  // pas attribute selectie toe en classificeer met gekozen methode voor numFeatures = het aantal features
 				  Classifier cls = new AttributeSelectedClassifier();
-				  // String[] options = weka.core.Utils.splitOptions("-x 2 -E \"weka.attributeSelection.InfoGainAttributeEval\" -S \"weka.attributeSelection.Ranker -N "+numFeatures+"\" -W "+method);
-				  String[] options = weka.core.Utils.splitOptions("-x 2 ...");
-				  cls.setOptions(options);
+				  String[] optionsArray = weka.core.Utils.splitOptions(options+" -N "+numFeatures+"\" -W "+method);
+				  cls.setOptions(optionsArray);
 				  cls.buildClassifier(train);
 				  
 				  Evaluation eval = new Evaluation(train);
@@ -70,7 +92,17 @@ public class ExpFeatureSet {
 		    
 		}
 		
-		Files.writeFile(exp + "/results.dat", output);
+		Files.writeFile(exp + "results.dat", output);
+		
+		String log = "Experiment voor feature set gedaan" + "\n"
+				+ "Locatie van resultaten: " + exp + "\n"
+				+ "Gebruikte dataset: " + data + "\n"
+				+ "Gebruikte methode: " + method + "\n"
+				+ "Opties voor AttributeSelectedClassifier: " + options;
+		
+		Files.writeFile(exp + "log.txt", log);
+		
+		System.out.println(log);
 		
 	}
 	
