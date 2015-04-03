03c3ede3784b16ec748e3eb173e22c634eb86faa
arnedb <arne.debrabandere@student.kuleuven.be>
2015-04-03 16:24:17 +0200

    enkele resultaten

--- evaluateSequences.sh
+++ evaluateSequences.sh
@@ -1,3 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
-java -jar ActivityRecognition.jar eval ./Sequenties/Models/all-features.vote-kstar-randomforest.model ./Sequenties/sequentie1_m_r_labels.csv ./Sequenties/sequentie1_m_r_20150210131306.log 4.0 0.75 ./Settings/settings.json 0.3
+declare -a models=("all-features.vote-kstar-randomforest") # zonder extensies
+
+# bestandsnaam van log-bestand en csv-bestand voor labels moet zelfde zijn, op extensie na!
+declare -a sequenties=("sequentie1_m_r_20150210131306") # zonder extensies
+
+for sequentie in "${sequenties[@]}"
+do
+
+    for size in 4.0 6.0 8.0
+    do
+        for overlap in 0.0 0.25 0.5 0.75
+        do
+            for cutoff in 0.5 0.4 0.3 0.2 0.1 0.0
+            do
+                for model in "${models[@]}"
+                do
+
+                    java -jar ActivityRecognition.jar eval ./Sequenties/Models/$model.model ./Sequenties/$sequentie.csv ./Sequenties/$sequentie.log $size $overlap ./Settings/settings.json $cutoff > ./Sequenties/Resultaten/$sequenties-$model-$size-$overlap-$cutoff.output.txt
+
+                    cp ./predictioncsv.csv ./Sequenties/Resultaten/$sequenties-$model-$size-$overlap-$cutoff.predictioncsv.csv
+
+                done
+            done
+        done
+    done
+
+done
